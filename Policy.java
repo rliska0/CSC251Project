f97e7b2516ecdddf00889fd152c4643c1a7f7d5d
rliska0 <rliska00@email.cpcc.edu>
2024-04-08 20:11:25 -0400

    Moved to PolicyHolder
    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -5,12 +5,6 @@ public class Policy
 {
    private int number;
    private String providerName;
-   private String firstName;
-   private String lastName;
-   private int age;
-   private String smoker;
-   private double height;
-   private double weight;
    
    public Policy() {
    }
@@ -19,22 +13,10 @@ public class Policy
       Constructor that accepts arguments
       @param number The policy number of the user
       @param providerName The name of the name of the provider for the policy
-      @param firstName The first name of the user
-      @param lastName The last name of the use
-      @param age The age of the user
-      @param smoker Whether or not the user is a smoker
-      @param height The height of the user
-      @param weight The weight of the user
    */
-   public Policy(int number, String providerName, String firstName, String lastName, int age, String smoker, double height, double weight) {
+   public Policy(int number, String providerName) {
    this.number = number;
    this.providerName = providerName;
-   this.firstName = firstName;
-   this.lastName = lastName;
-   this.age = age;
-   this.smoker = smoker;
-   this.height = height;
-   this.weight = weight;
    }
    
    /**
@@ -52,48 +34,6 @@ public class Policy
    }
    
    /**
-      @param firstName The first name of the user
-   */
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
-   
-   /**
-      @param lastName The last name of the user
-   */
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
-   
-   /**
-      @param age The age of the user
-   */
-   public void setAge(int age) {
-      this.age = age;
-   }
-   
-   /**
-      @param smoker Whether or not the user is a smoker
-   */
-   public void setSmoker(String smoker) {
-      this.smoker = smoker;
-   }
-   
-   /**
-      @param height The height of the user
-   */
-   public void setHeight(double height) {
-      this.height = height;
-   }
-   
-   /**
-      @param weight The weight of the user
-   */
-   public void setWeight(double weight) {
-      this.weight = weight;
-   }
-   
-   /**
       @return number The policy number of the user
    */
    public int getNumber() {
@@ -105,75 +45,25 @@ public class Policy
    */
    public String getProviderName() {
       return providerName;
-   }
-   
-   /**
-      @return firstName The first name of the user
-   */
-   public String getFirstName() {
-      return firstName;
-   }
-   
-   /**
-      @return lastName The last name of the user
-   */
-   public String getLastName() {
-      return lastName;
-   }
-   
-   /**
-      @return age The age of the user
-   */
-   public int getAge() {
-      return age;
-   }
-   
-   /**
-      @return smoker Whether or not the user is a smoker
-   */
-   public String getSmoker() {
-      return smoker;
-   }
-   
-   /**
-      @return height The height of the user
-   */
-   public double getHeight() {
-      return height;
-   }
-   
-   /**
-      @return weight The weight of the user
-   */
-   public double getWeight() {
-      return weight;
-   }
-   
-   /**
-      Calculates the bmi of the user
-      @return The bmi of the user
-   */
-   public double bmi() {
-      return ((weight*703)/(height*height));
-   }
-   
+   }   
    
    /**
       Calculates the policy price
+      @param policyHolder The PolicyHolder object containing height and weight
       @return The policy price of the user
    */
-   public double policyPrice() {
+   public double policyPrice(PolicyHolder policyHolder) {
       double policyPrice = 600;
-      if (age > 50) {
+      if (policyHolder.getAge() > 50) {
          policyPrice += 75;
       }
       
-      if (smoker.equals("smoker")) {
+      if (policyHolder.getSmoker().equalsIgnoreCase("smoker")) {
          policyPrice +=100; 
       }
       
-      if (bmi() > 35) {
-         policyPrice += ((bmi()-35)*20);
+      if (policyHolder.bmi() > 35) {
+         policyPrice += ((policyHolder.bmi()-35)*20);
       }
       return policyPrice;
    }
